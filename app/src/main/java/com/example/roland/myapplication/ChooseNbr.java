0a43f3cd4afc3986cc50377f199351d0e1d9a4be
Raphaël Benalouane <r.benalouane@laposte.net>
2016-08-04 14:57:07 +0200

    Add a function in .xml and .java files for choose_nbr activity that valorise nbrResistance variable with keybord entry.

--- app/src/main/java/com/example/roland/myapplication/ChooseNbr.java
+++ app/src/main/java/com/example/roland/myapplication/ChooseNbr.java
@@ -3,7 +3,11 @@ package com.example.roland.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ChooseNbr extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class ChooseNbr extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_nbr);
     }
+
+    EditText editText = (EditText) findViewById(R.id.enter_number);
+    String nbrResistance = editText.getText().toString();
 
     /**Calling Home */
     public void callHome(View view){
