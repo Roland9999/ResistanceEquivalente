88658c9747186987af0430872b420a8db7fc21cd
Roland ALIMA <alimaroland85@yahoo.com>
2016-08-04 09:50:01 +0200

    Changing tolerance (spinner6) list of colors

--- app/src/main/java/com/example/roland/myapplication/AddAResistance.java
+++ app/src/main/java/com/example/roland/myapplication/AddAResistance.java
@@ -57,8 +57,9 @@ public class AddAResistance extends AppCompatActivity {
 
         Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
-                R.array.colors, android.R.layout.simple_spinner_item);
+        /*ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
+                R.array.colors, android.R.layout.simple_spinner_item);*/
+        CostomSpinnerAdapter adapter6 = new CostomSpinnerAdapter(this, ColorsToleranceValues.ringsToleranceColors());
         // Specify the layout to use when the list of choices appears
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
