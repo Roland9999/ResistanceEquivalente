f1890aff3f9f3db05854deaf2ef140d746217e05
Raphaël Benalouane <r.benalouane@laposte.net>
2016-08-05 16:03:49 +0200

    test elements in comment.

--- app/src/main/java/com/example/roland/myapplication/ChooseNbr.java
+++ app/src/main/java/com/example/roland/myapplication/ChooseNbr.java
@@ -18,16 +18,43 @@ public class ChooseNbr extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.enter_number);
         //create a variable for save the number written by the user.
         String nbrResistance = editText.getText().toString();
-    }
-
-    public void onValidate(View view) {
 
     }
 
+    /*public String enterNbr() {
+
+        //protected void onRestart() {}
+        return nbrResistance;
+    }*/
 
 
-    /**Calling Home */
-    public void callHome(View view){
+
+
+    public void onValidate(View view, String nbrResistance) {
+
+    }
+       /* nbrResistance = enterNbr();
+        int nbrResInteger = Integer.parseInt(nbrResistance);
+        int compter = 0;
+        boolean restart = true;
+        do {
+            if (compter == nbrResInteger) {
+                restart = false;
+                Intent intent = new Intent(this, Resume.class);
+                this.startActivity(intent);
+            } else {
+                compter++;
+                onRestart();
+            }
+        } while (restart);
+
+    }*/
+
+
+    /**
+     * Calling Home
+     */
+    public void callHome(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         this.startActivity(intent);
     }
