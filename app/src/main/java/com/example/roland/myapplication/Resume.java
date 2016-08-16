1eef1cc41461de76d32cb1c175c9591dbf022614
Roland ALIMA <alimaroland85@yahoo.com>
2016-08-16 09:50:08 +0200

    implementing resume class

--- app/src/main/java/com/example/roland/myapplication/Resume.java
+++ app/src/main/java/com/example/roland/myapplication/Resume.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class Resume extends AppCompatActivity {
+
+    List<String> valuesList = ResitancesValuesList.getValuesList();
+    ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        myListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, valuesList);
+        myListView.setAdapter(adapter);
+
+        //int val = ChooseComplexType.getCircuitType();
     }
 
     /**Calling activity AddAResistance */
