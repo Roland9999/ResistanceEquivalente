2b4d52e5293c2ccd0a0709637d420b441671ba7d
Roland ALIMA <alimaroland85@yahoo.com>
2016-08-26 15:22:29 +0200

    adding unity of resistances values

--- app/src/main/java/com/example/roland/myapplication/MainActivity.java
+++ app/src/main/java/com/example/roland/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.roland.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,4 +27,25 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Récupération de l'orientation de l'écran
+        int rotationEcran = this.getWindowManager().getDefaultDisplay().getRotation();
+
+        // Et positionner ainsi le nombre de degrés de rotation
+        int degrees = 0;
+        switch (rotationEcran) {
+            case Surface.ROTATION_0: degrees = 0;
+                break;
+            case Surface.ROTATION_90: degrees = 90;
+                break;
+            case Surface.ROTATION_180: degrees = 180;
+                break;
+            case Surface.ROTATION_270: degrees = 270;
+                break;
+        }
+
+        Log.d ("test orientation", "=" + degrees);
+    }
 }
