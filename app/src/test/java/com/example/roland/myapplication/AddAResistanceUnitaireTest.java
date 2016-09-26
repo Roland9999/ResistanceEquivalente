ec5500bd264fc2003ec8741573597169f16b2615
Raphaël Benalouane <r.benalouane@laposte.net>
2016-09-26 11:12:09 +0200

    25 sept 2016 11h11 commit for merge

---
+++ app/src/test/java/com/example/roland/myapplication/AddAResistanceUnitaireTest.java
@@ -0,0 +1,27 @@
+package com.example.roland.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Benalouane_R on 09/09/2016.
+ */
+public class AddAResistanceUnitaireTest {
+
+   /* private AddAResistance addAResistance = new AddAResistance();
+
+
+    @Test
+    public void testOfComputeValueMethode(){
+        double[] tabDouble = {0.0,0.0,0.0,0.0,20.0,0.0};
+        assertEquals(0, addAResistance.computeValue(3, tabDouble));
+    }
+
+    @Test
+    public void testOfComputeValueMethode(){
+        double[] tabDouble = {0.0,0.0,0.0,0.0,20.0,0.0};
+        assertEquals(0, addAResistance.computeValue(3, tabDouble));
+    }
+*/
+}
