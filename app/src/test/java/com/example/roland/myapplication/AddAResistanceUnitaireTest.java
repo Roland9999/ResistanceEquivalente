90bdae7410a218feec62dee8d4a25b214d975739
Raphaël Benalouane <r.benalouane@laposte.net>
2016-09-27 13:26:03 +0200

    add news unitary test

--- app/src/test/java/com/example/roland/myapplication/AddAResistanceUnitaireTest.java
+++ app/src/test/java/com/example/roland/myapplication/AddAResistanceUnitaireTest.java
@@ -3,25 +3,38 @@ package com.example.roland.myapplication;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by Benalouane_R on 09/09/2016.
  */
 public class AddAResistanceUnitaireTest {
 
-   /* private AddAResistance addAResistance = new AddAResistance();
+   private AddAResistance addAResistance = new AddAResistance();
 
 
     @Test
-    public void testOfComputeValueMethode(){
+    public void testOfComputeValueMethode() throws Exception{
         double[] tabDouble = {0.0,0.0,0.0,0.0,20.0,0.0};
-        assertEquals(0, addAResistance.computeValue(3, tabDouble));
+        assertEquals(0, addAResistance.computeValue(3, tabDouble), 0);
     }
 
     @Test
-    public void testOfComputeValueMethode(){
-        double[] tabDouble = {0.0,0.0,0.0,0.0,20.0,0.0};
-        assertEquals(0, addAResistance.computeValue(3, tabDouble));
+    public void testOfComputeValueMethode2() throws Exception{
+        double[] tabDouble = {9.0,5.0,0.0,2.0,20.0,0.0};
+        assertEquals(9500.0, addAResistance.computeValue(3, tabDouble), 0);
     }
-*/
+
+    @Test
+    public void testOfComputeValueMethode3() throws Exception{
+        double[] tabDouble = {7.0,3.0,1.0,1.0,10.0,0.0};
+        assertEquals(7310.0, addAResistance.computeValue(5, tabDouble), 0);
+        //assertEquals(10.0, addAResistance.takingSpinnerValueRes5Rings(tabDouble));
+    }
+
+    @Test
+    public void testOfComputeValueMethode4() throws Exception {
+        double[] tabDouble = {7.0,3.0,5.0,-2.0,10.0,5.0};
+        assertNotEquals(7.310, addAResistance.computeValue(6, tabDouble), 0);
+    }
 }
